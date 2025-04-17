c831f2169879d5e26423044fee3126e3bca136f7
Ornella Gigante <ornella-gigante@hotmail.com.ar>
2025-04-18 01:29:24 +0200

    feat: Declare variables for all white and black piano keys in MainActivity

--- app/src/main/java/es/ifp/melodykeys/MainActivity.java
+++ app/src/main/java/es/ifp/melodykeys/MainActivity.java
@@ -1,6 +1,7 @@
 package es.ifp.melodykeys;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.HorizontalScrollView;
 
 import androidx.activity.EdgeToEdge;
@@ -13,6 +14,21 @@ public class MainActivity extends AppCompatActivity {
 
     private HorizontalScrollView scrollView;
 
+    // White keys
+    private Button keyC3, keyD3, keyE3, keyF3, keyG3, keyA3, keyB3;
+    private Button keyC4, keyD4, keyE4, keyF4, keyG4, keyA4, keyB4;
+    private Button keyC5, keyD5, keyE5, keyF5, keyG5, keyA5, keyB5;
+    private Button keyC6, keyD6, keyE6, keyF6, keyG6, keyA6, keyB6;
+    private Button keyC7, keyD7, keyE7, keyF7, keyG7, keyA7, keyB7;
+
+    // Black keys (example for 3 octaves, add more as needed)
+    private Button keyCs3, keyDs3, keyFs3, keyGs3, keyAs3;
+    private Button keyCs4, keyDs4, keyFs4, keyGs4, keyAs4;
+    private Button keyCs5, keyDs5, keyFs5, keyGs5, keyAs5;
+    private Button keyCs6, keyDs6, keyFs6, keyGs6, keyAs6;
+    private Button keyCs7, keyDs7, keyFs7, keyGs7, keyAs7;
+    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
 
         // To inizialize the scroll view
         scrollView = findViewById(R.id.scrollView);
-        
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);

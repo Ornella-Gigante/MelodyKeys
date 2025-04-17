76ac61f36fe782c953eced6f7a8c29ce9dd93cad
Ornella Gigante <ornella-gigante@hotmail.com.ar>
2025-04-18 01:18:44 +0200

    feat: Initialize scrollView in MainActivity onCreate method

--- app/src/main/java/es/ifp/melodykeys/MainActivity.java
+++ app/src/main/java/es/ifp/melodykeys/MainActivity.java
@@ -1,6 +1,8 @@
 package es.ifp.melodykeys;
 
 import android.os.Bundle;
+import android.widget.HorizontalScrollView;
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -9,11 +11,17 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private HorizontalScrollView scrollView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        // To inizialize the scroll view
+        scrollView = findViewById(R.id.scrollView);
+        
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);

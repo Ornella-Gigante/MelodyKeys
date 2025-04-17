ec767eec646ac51fde8f8668889e85d25c6f9c93
Ornella Gigante <ornella-gigante@hotmail.com.ar>
2025-04-18 01:50:02 +0200

    feat: Add and initialize all piano key label TextViews in MainActivity

--- app/src/main/java/es/ifp/melodykeys/MainActivity.java
+++ app/src/main/java/es/ifp/melodykeys/MainActivity.java
@@ -3,6 +3,7 @@ package es.ifp.melodykeys;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.HorizontalScrollView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -27,6 +28,14 @@ public class MainActivity extends AppCompatActivity {
     private Button keyCs5, keyDs5, keyFs5, keyGs5, keyAs5;
     private Button keyCs6, keyDs6, keyFs6, keyGs6, keyAs6;
     private Button keyCs7, keyDs7, keyFs7, keyGs7, keyAs7;
+
+    // TextViews
+    private TextView labelC3, labelD3, labelE3, labelF3, labelG3, labelA3, labelB3;
+    private TextView labelC4, labelD4, labelE4, labelF4, labelG4, labelA4, labelB4;
+    private TextView labelC5, labelD5, labelE5, labelF5, labelG5, labelA5, labelB5;
+    private TextView labelC6, labelD6, labelE6, labelF6, labelG6, labelA6, labelB6;
+    private TextView labelC7, labelD7, labelE7, labelF7, labelG7, labelA7, labelB7;
+
 
 
     @Override
@@ -109,6 +118,44 @@ public class MainActivity extends AppCompatActivity {
         keyFs7 = findViewById(R.id.keyFs7);
         keyGs7 = findViewById(R.id.keyGs7);
         keyAs7 = findViewById(R.id.keyAs7);
+
+        //  TextViews
+        labelC3 = findViewById(R.id.labelC3);
+        labelD3 = findViewById(R.id.labelD3);
+        labelE3 = findViewById(R.id.labelE3);
+        labelF3 = findViewById(R.id.labelF3);
+        labelG3 = findViewById(R.id.labelG3);
+        labelA3 = findViewById(R.id.labelA3);
+        labelB3 = findViewById(R.id.labelB3);
+        labelC4 = findViewById(R.id.labelC4);
+        labelD4 = findViewById(R.id.labelD4);
+        labelE4 = findViewById(R.id.labelE4);
+        labelF4 = findViewById(R.id.labelF4);
+        labelG4 = findViewById(R.id.labelG4);
+        labelA4 = findViewById(R.id.labelA4);
+        labelB4 = findViewById(R.id.labelB4);
+        labelC5 = findViewById(R.id.labelC5);
+        labelD5 = findViewById(R.id.labelD5);
+        labelE5 = findViewById(R.id.labelE5);
+        labelF5 = findViewById(R.id.labelF5);
+        labelG5 = findViewById(R.id.labelG5);
+        labelA5 = findViewById(R.id.labelA5);
+        labelB5 = findViewById(R.id.labelB5);
+        labelC6 = findViewById(R.id.labelC6);
+        labelD6 = findViewById(R.id.labelD6);
+        labelE6 = findViewById(R.id.labelE6);
+        labelF6 = findViewById(R.id.labelF6);
+        labelG6 = findViewById(R.id.labelG6);
+        labelA6 = findViewById(R.id.labelA6);
+        labelB6 = findViewById(R.id.labelB6);
+        labelC7 = findViewById(R.id.labelC7);
+        labelD7 = findViewById(R.id.labelD7);
+        labelE7 = findViewById(R.id.labelE7);
+        labelF7 = findViewById(R.id.labelF7);
+        labelG7 = findViewById(R.id.labelG7);
+        labelA7 = findViewById(R.id.labelA7);
+        labelB7 = findViewById(R.id.labelB7);
+
     }
 
 }

f7b5fde5edc1160b32daf83db5e2cfb17866a802
Ornella Gigante <ornella-gigante@hotmail.com.ar>
2025-04-20 21:48:36 +0200

    chore: Disable gotoTestActivity method call in MainActivity

--- app/src/main/java/es/ifp/melodykeys/MainActivity.java
+++ app/src/main/java/es/ifp/melodykeys/MainActivity.java
@@ -51,7 +51,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         // Test
 
-        gotoTestActivity();
+        // gotoTestActivity();
 
         // All keys init
         initializeAllPianoKeys();
